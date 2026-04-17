7f135286e4520c216d3bd06b00489a3ec81c37d7
Gabriel Caetano <gabriel.henrique.cm.2007@gmail.com>
2026-04-17 16:21:49 -0300

    Criação do repositório colocando atividade inicial de POO

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        Scanner scan = new Scanner(System.in);
+
+        Person homem = new Person();
+        homem.setNome("Gabriel");
+        homem.setIdade(18);
+
+        Person mulher = new Person();
+        mulher.setNome("Isabeli");
+        mulher.setIdade(19);
+
+        System.out.printf("Nome: %s\n" + "Idade: %d\n", homem.getNome(), homem.getIdade());
+        System.out.printf("Nome: %s\n" + "Idade: %d\n", mulher.getNome(), mulher.getIdade());
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    //privamos as propriedades para que somente o que estiver dentro do PERSON consiga utiliza-las.
+    private String nome;
+    private int idade;
+
+    // Criamos os get para poder receber os nomes/idades
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+    //criamos os set para poder DEFINIR os nomes/idades e o get receber.
+
+    public void setNome(String nome){
+        this.nome = nome; //Quando a conflito de nome de variaveis usamos o "this" para referenciar a variavel da CLASSE PERSON
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+}
